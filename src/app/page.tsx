e778347d31f8401075a62074c41a330de0855e96
Monika Zyznowska <monika.zyznowska@elpassion.pl>
2023-11-16 11:01:49 +0100

    Move google font to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 import Image from "next/image";
-import { Poppins } from "next/font/google";
-
-const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
   return (
@@ -13,12 +10,12 @@ export default function Home() {
         height={130}
         priority
       />
-      <div className={poppins.className}>
-        <p className="text-center">
+      <div className="text-center">
+        <p>
           Hjelper deg å trene dine mentale muskler. Der du er, når du trenger
           det.
         </p>
-        <p className="text-center">Kontakt: Thara Østensvik +47 934 09 651</p>
+        <p>Kontakt: Thara Østensvik +47 934 09 651</p>
       </div>
     </main>
   );
