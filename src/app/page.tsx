d1fd96dc6e6a15d3989a29d95686d68df4d0cc4f
Monika Zyznowska <monika.zyznowska@elpassion.pl>
2023-11-20 13:56:10 +0100

    Change coming soon place

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-8">
-      <div className="flex flex-col gap-7 items-center">
-        <Image
-          src="/logotype.svg"
-          alt="MyMoods Logo"
-          width={414}
-          height={130}
-          priority
-        />
-        <div className="text-center">
-          <p>
-            Hjelper deg å trene dine mentale muskler. Der du er, når du trenger
-            det.
-          </p>
-          <p>Coming soon...</p>
-        </div>
+      <p className="text-center font-extrabold text-2xl">Coming soon</p>
+      <Image
+        src="/logotype.svg"
+        alt="MyMoods Logo"
+        width={414}
+        height={130}
+        priority
+      />
+      <div className="text-center">
+        <p>
+          Hjelper deg å trene dine mentale muskler. Der du er, når du trenger
+          det.
+        </p>
+        <p>Kontakt: Thara Østensvik +47 934 09 651</p>
       </div>
-      <p className="text-center">Kontakt: Thara Østensvik +47 934 09 651</p>
     </main>
   );
 }
